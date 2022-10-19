f09034caa4de20f0cdb86ddf77691c0e70764171
iremerdgn9 <irmerdgn06@gmail.com>
2022-10-19 14:44:33 +0300

    initial commit

---
+++ hello-world.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World !");
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+
